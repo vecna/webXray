8b0261713d6fabb1e732b5a78c58978d487734b0
Claudio <claudio@tracking.exposed>
2019-05-01 14:56:13 +0200

    network.csv contains Date as column

--- webxray/Analyzer.py
+++ webxray/Analyzer.py
@@ -1304,9 +1304,9 @@ class Analyzer:
 			if item[1]:
 				# this condition has to specify != None, b/c otherwise it will skip values of 0
 				if item[2] != None:
-					csv_rows.append((time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()), item[0],item[1],self.domain_owners[item[2]]['owner_name'],self.domain_owners[item[2]]['country']))
+					csv_rows.append(( time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()), item[0],item[1],self.domain_owners[item[2]]['owner_name'],self.domain_owners[item[2]]['country']))
 				else:
-					csv_rows.append((item[0],item[1],'Unknown',''))
+					csv_rows.append(( time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()), item[0],item[1],'Unknown',''))
 		self.write_csv('network.csv', csv_rows)
 	# generate_network_report
 # Analyzer
